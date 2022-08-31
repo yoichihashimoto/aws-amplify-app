a088cbf0eba0e43126e7423000b2e2ba1bbd385c
YoichiHashimoto <yoichi.creative0223@gmail.com>
2022-08-31 22:50:35 +0900

    amplify add

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "awstutorialwithreact81bc850c": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
